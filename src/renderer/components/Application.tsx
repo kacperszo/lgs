b51846b6322cf892be06cc4bb9751ce4244d0931
Kacper Szot <kacperszot1@gmail.com>
2021-11-28 18:01:47 +0100

    init

---
+++ src/renderer/components/Application.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './Application.less';
+
+const Application: React.FC = () => {
+  return <h1>Siema</h1>;
+};
+
+export default hot(module)(Application);
